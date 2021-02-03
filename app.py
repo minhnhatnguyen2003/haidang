c42a16448bf2a8a7b14feab6f2965d6e01654d27
minhnhatnguyen2003 <47096001+minhnhatnguyen2003@users.noreply.github.com>
2021-02-03 12:04:55 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from wordcloud import WordCloud, STOPWORDS
+import imageio
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image
+
+
+st.title("Hello Đăng :point_right: :point_left:")
+
+# Import packages
+import numpy as np
+from PIL import Image
+
+text = 'HaiDang,Confident,Smart,Talented,Passionate,Humble,LowkeyCrazy,Horny,Caring,FutureDoctor,Kind,Memelord,Uvuvwevwevwe onyetenyevwe ugwemuhwem osas, 1_Kilo_of_feathers, Why_are_u_gay'
+
+
+if st.button("Press me first" ):
+        with st.spinner("Appreciation post :eyes:"):
+
+          st.subheader("One: I admire your light and your warmth; they make everyday brighter.")
+
+          st.subheader("Two: I admire your drive and intelligence. I have no doubt that you'll achieve great things.")
+
+          st.subheader("Three: I love your energy.How the hell can you do a morning class, basketball and football? You amaze me in every way")
+
+          st.image("https://i.pinimg.com/564x/0b/70/37/0b7037498e232cb64a8185b55eccb226.jpg", use_column_width=True, caption = 'I made this WordCloud abt what I think of you')
+          st.image("https://i.pinimg.com/originals/13/aa/42/13aa42c14657034f5e76981bb8ce87ae.png?epik=dj0yJnU9U1l3UWVlMWlCRmVDdWduVUlEWExVMUhKQTJOLXR4VzQmcD0wJm49WDhsSjhBZ0dYNk9rVm95cTRyLUlWZyZ0PUFBQUFBR0FhS1Vz.png", use_column_width=True, caption = 'I also made this meme code with sum code ofc ')
+
+
+
+if st.button("Hit me second"):
+            st.subheader("I want to be better with you, and I know that I can help you too. So let me... ask this corny question: ")
+            st.subheader("Will you be my mập địt? Send your answer on Messenger :))")
+            st.write("If yes, send me :pig_nose:")
+            st.write("If no, send me :japanese_ogre:")
+            st.write("If you need more time, send me :clock1: but you'll have max 3hours")
+
+
+if st.button("Smash me last for a surprise"):
+  st.video('https://youtu.be/p--GfVXfNa0')
